128e70d1b99e150320b8fbcbe7bb4cffbe145c6b
Juan Miguel Jimeno <jimenojmm@gmail.com>
2021-04-14 22:48:03 +0800

    added tf lite converter

---
+++ semantic_segmentation/tf_lite_converter.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from coco_image import CocoImage
+
+# A generator that provides a representative dataset
+def representative_data_gen():
+    annotation_file = '/home/juan/DATASET/HOUGANG/label.json'
+    classes = ['Path']
+    image_size = 128
+    coco_image = CocoImage(annotation_file, classes)
+    dataset_list = coco_image.image_ids
+    for i in range(100):
+        image = next(iter(dataset_list))
+        image = coco_image.get_image_string(image)
+        image = tf.io.read_file(image)
+        image = tf.io.decode_jpeg(image, channels=3)
+        image = tf.image.resize(image, [image_size, image_size])
+        image = tf.cast(image / 255., tf.float32)
+        image = tf.expand_dims(image, 0)
+        yield [image]
+
+model = load_model('semantic_segmentation.h5')
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.experimental_new_converter = False
+
+# This enables quantization
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+# This sets the representative dataset for quantization
+converter.representative_dataset = representative_data_gen
+# This ensures that if any ops can't be quantized, the converter throws an error
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+# For full integer quantization, though supported types defaults to int8 only, we explicitly declare it for clarity.
+converter.target_spec.supported_types = [tf.int8]
+# These set the input and output tensors to uint8 (added in r2.3)
+converter.inference_input_type = tf.uint8
+converter.inference_output_type = tf.uint8
+tflite_model = converter.convert()
+
+with open('semantic_segmentation.tflite', 'wb') as f:
+    f.write(tflite_model)
